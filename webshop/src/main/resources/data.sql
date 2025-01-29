2a69c8eadc1e3dccf76f0f837d8ee141e41a1a35
Cheatahh <72265408+Cheatahh@users.noreply.github.com>
2025-01-29 18:08:38 +0100

    [+] Added 10 product images [~] Adjusted product pricing

--- webshop/src/main/resources/data.sql
+++ webshop/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO products
-    ("id", "created_at", "name", "description", "price", "category", "imageUrl")
+("id", "created_at", "name", "description", "price", "category", "image_url")
 VALUES
     (
         1,
         '2025-01-01 00:00:00',
         'Aria Curve Chair',
         'A simple, yet elegant wooden chair with a natural finish, made from polished oak. The chair features a slightly curved backrest for comfort, four sturdy, tapered legs for stability, and a smooth, contoured seat that enhances both style and functionality. Perfect for dining rooms or minimalist interiors.',
-        21900,
+        11900,
         'Dining',
-        'https://<todo url>'
+        'images/AriaCurveChair.webp'
     ),
     (
         2,
@@ -17,52 +17,52 @@ VALUES
         'A modern wooden chair with a unique and artistic design, featuring a natural walnut finish. The chair has a curved ergonomic backrest and a padded seat upholstered in soft, durable fabric for added comfort. Its sleek lines and contemporary aesthetic make it an ideal choice for stylish office areas or accent seating.',
         29900,
         'Office',
-        'https://<todo url>'
+        'images/OakridgeClassicChair.webp'
     ),
     (
         3,
         '2025-01-01 00:00:00',
         'Rustic Ladderback Chair',
         'A rustic wooden chair with a weathered, distressed finish, made from reclaimed wood. The chair features a high ladder back design, a sturdy rectangular shape, and a spacious, flat seat for added comfort. Its robust construction makes it perfect for farmhouse-inspired dining rooms or cozy, rustic spaces.',
-        21900,
+        12900,
         'Dining',
-        'https://<todo url>'
+        'images/RusticLadderbackChair.webp'
     ),
     (
         4,
         '2025-01-01 00:00:00',
         'Velvet Haven Accent Chair',
-        'A luxurious accent chair with a plush velvet upholstery and gold-finished legs. Adds a touch of elegance to any living room or bedroom.',
+        'A luxurious accent chair with a plush upholstery and gold-finished leg-tips. Adds a touch of elegance to any living room or bedroom.',
         37900,
         'Bedroom',
-        'https://<todo url>'
+        'images/VelvetHavenAccentChair.webp'
     ),
     (
         5,
         '2025-01-01 00:00:00',
         'Lunar Loft Bookshelf',
-        'A sleek, modern bookshelf with five open shelves and a powder-coated steel frame. Ideal for organizing books or displaying decorative items.',
-        41900,
+        'A sleek, modern bookshelf with open shelving units and a powder-coated steel frame. Ideal for organizing books or displaying decorative items.',
+        61900,
         'Storage',
-        'https://<todo url>'
+        'images/LunarLoftBookshelf.webp'
     ),
     (
         6,
         '2025-01-01 00:00:00',
         'Harbor Breeze King Bed Frame',
-        'A coastal-inspired king-size bed frame with a weathered white finish and slatted headboard. Offers both style and comfort.',
-        189900,
+        'A coastal-inspired king-size bed frame with a weathered white finish and couchy headboard. Offers both style and comfort.',
+        179900,
         'Bedroom',
-        'https://<todo url>'
+        'images/HarborBreezeKingBedFrame.webp'
     ),
     (
         7,
         '2025-01-01 00:00:00',
         'Aspen Oak Dining Table',
-        'A sturdy dining table made from solid oak, featuring a natural wood grain finish and rounded edges. Perfect for hosting family dinners or gatherings.',
+        'A sturdy dining table made from solid oak, featuring a natural wood grain finish. Perfect for hosting family dinners or gatherings.',
         79900,
         'Dining',
-        'https://<todo url>'
+        'images/AspenOakDiningTable.webp'
     ),
     (
         8,
@@ -71,24 +71,32 @@ VALUES
         'A compact writing desk crafted from reclaimed wood with built-in drawers for storage. Combines rustic charm with functionality.',
         69900,
         'Office',
-        'https://<todo url>'
+        'images/WillowBranchWritingDesk.webp'
     ),
     (
         9,
         '2025-01-01 00:00:00',
         'Urban Nest Coffee Table',
-        'A stylish coffee table with a tempered glass top and a geometric metal base. The perfect centerpiece for a modern living room.',
-        59900,
+        'A stylish coffee table with a tempered glass top and a twisted wooden base. The perfect centerpiece for a modern living room.',
+        89900,
         'Living Room',
-        'https://<todo url>'
+        'images/UrbanNestCoffeeTable.webp'
     ),
     (
         10,
         '2025-01-01 00:00:00',
         'Urban Nest Lounge Sofa',
-        'A stylish sofa featuring soft, high-density cushions and a low-profile frame. The perfect addition for a modern living room.',
-        119900,
+        'A stylish sofa featuring soft, high-density cushions and a rounded wooden frame. The perfect addition for a modern living room.',
+        219900,
         'Living Room',
-        'https://<todo url>'
+        'images/UrbanNestLoungeSofa.webp'
     )
-ON CONFLICT ("id") DO NOTHING;
+ON CONFLICT ("id") DO UPDATE
+SET
+    "created_at" = EXCLUDED."created_at",
+    "name" = EXCLUDED."name",
+    "description" = EXCLUDED."description",
+    "price" = EXCLUDED."price",
+    "category" = EXCLUDED."category",
+    "image_url" = EXCLUDED."image_url"
+;
