d81212274c6a07b36ee21c5968499c18c4ad1ada
Cheatahh <72265408+Cheatahh@users.noreply.github.com>
2025-02-02 17:39:13 +0100

    [+] Added configuration parameter for S3 ImageDB [+] Added S3 ImageDB to CORS [~] Product imageUrl behavior to build url from environment parameters.

--- webshop/src/main/java/com/lowtech/webshop/model/Product.java
+++ webshop/src/main/java/com/lowtech/webshop/model/Product.java
@@ -3,6 +3,7 @@ package com.lowtech.webshop.model;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.core.env.Environment;
 
 /**
  * @author Paul Barthel
@@ -10,16 +11,28 @@ import lombok.Setter;
  * @time 14:15
  */
 @Entity
-@Getter
-@Setter
 public class Product extends AbstractEntity {
 
+    @Getter @Setter
     private String name;
+
+    @Getter @Setter
     private String description;
 
-    // cents
-    private int price;
+    @Getter @Setter
+    private int price; // cents
 
+    @Getter @Setter
     private String category;
-    private String imageUrl;
+
+    private String imageUrl; // relative image path in bucket
+
+    public String getImageUrl(Environment environment) {
+        // prefix with current s3 bucket
+        return environment.getProperty("env.data.imageS3url") + imageUrl;
+    }
+    public void setImageUrl(String url) {
+        // remove base url before storage
+        imageUrl = url.substring(url.lastIndexOf('/') + 1);
+    }
 }
